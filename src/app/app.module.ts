8dccf6663942fbebc34da5037b2fe8599980ef8b
Cristian Motta <cdmottah@gmail.com>
2020-07-10 13:50:28 -0500

    New proyect Sellers already created, using bootstrap and footer, header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { SellerComponent } from './seller/seller.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashBoardComponent,
+    SellerComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
